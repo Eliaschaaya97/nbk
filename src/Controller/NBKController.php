e3aa4800c538648dd2b052c2f75f1e0b3b6e3c4f
Elias Chaaya <E.chaaya@elbaridgroup.local>
2024-05-21 15:31:24 +0300

    remove infoString,add devicetype

--- src/Controller/NBKController.php
+++ src/Controller/NBKController.php
@@ -34,30 +34,19 @@ class NBKController extends AbstractController
     #[Route('/nbk', name: 'app_react')]
     public function index(): Response
     {
-        $useragent = $_SERVER['HTTP_USER_AGENT'];
-        $_POST['infoString']="Mwx9v3bq3GNGIWBYFJ1f1PcdL3j8SjmsS6y+Hc76TEtMxwGjwZQJHlGv0+EaTI7c";
-
-        if (isset($_POST['infoString'])) {
-            $decrypted_string = GenerallServices::decrypt($_POST['infoString']); //['device'=>"aad", asdfsd]
-            $suyoolUserInfo = explode("!#!", $decrypted_string);
-            $devicetype = stripos($useragent, $suyoolUserInfo[1]);
-
-            if (true) {
-                $SuyoolUserId = $suyoolUserInfo[0];
-                //$this->session->set('suyoolUserId', $SuyoolUserId);
-                // $this->session->set('suyoolUserId', 155);
-
-                $parameters['deviceType'] = $suyoolUserInfo[1];
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $deviceType = "";
+        if (stripos($userAgent, 'Android') !== false) {
+            $deviceType = "Android";
+        }elseif (stripos($userAgent, 'iPhone') !== false || stripos($userAgent, 'iPad') !== false || stripos($userAgent, 'iPod') !== false) {
+            $deviceType = "Iphone";
+        }
+     
+        $parameters['deviceType'] = $deviceType;
 
                 return $this->render('nbk/index.html.twig', [
                     'parameters' => $parameters
                 ]);
-            }
-          
-        }  else {
-            return $this->render('ExceptionHandling.html.twig');
-        }
-
     }
     #[Route('/submit-data', name: 'submitData', methods: ['POST'])]
     public function submit(Request $request, UsersRepository $usersRepository, AddressRepository $addressRepository, WorkDetailsRepository $workDetailsRepository, BeneficiaryRightsOwnerRepository $beneficiaryRepository, PoliticalPositionDetailsRepository $politicalPositionRepository, FinancialDetailsRepository $financialRepository): JsonResponse {
